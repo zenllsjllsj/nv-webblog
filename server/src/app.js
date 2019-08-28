6a3242a30573810cd6c5d886020c884207e7ded7
Not <thrathepp@mfaster.com>
2019-08-28 15:22:16 +0700

    sqlite

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,9 @@
 let express = require('express')
 let bodyParser = require('body-parser')
 
+const {sequelize} = require('./models')
+const config = require('./config/config')
+
 const app = express()
 
 app.use(bodyParser.json())
@@ -17,12 +20,14 @@ app.get('/hello/:person', function (req,res) {
  res.send('sey hello with ' + req.params.person)
 })
 
-app.post('/hello', function (req, res){
-    res.send('OK you Post - ' + req.body.name)
-})
+app.post('/hello', function (req, res) {
+    res.send('OK you post - ' + req.body.name)
+   })
 
-let port = 8081
+let port = process.env.PORT || config.port
 
-app.listen(port, function () {
- console.log('server running on ' + port)
+   sequelize.sync({force: false}).then(() => {
+    app.listen(port, function () {
+    console.log('Server running on ' + port)
+    })
 })
